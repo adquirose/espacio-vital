31153b9c689d36b2862d4480d31dca28f225d845
Arturo Quiros <adquirose@gmail.com>
2022-07-18 16:48:34 -0400

    iniciando header, agregue tipografia al index.html, y logo

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo")
+/* Storing user's device details in a variable*/
+let details = navigator.userAgent;
+  
+/* Creating a regular expression 
+containing some mobile devices keywords 
+to search it in details string*/
+let regexp = /android|iphone|kindle|ipad/i;
+
+/* Using test() method to search regexp in details
+it returns boolean value*/
+let isMobileDevice = regexp.test(details);
+
+if (isMobileDevice) {
+    document.write("usando Movil");
+} else {
+    document.write("usando Desktop");
+}
